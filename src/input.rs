963e2fbe9ed0cc4c04bb1cf19690fcc246449df9
Artur Cruz <arturacruz@icloud.com>
2025-07-26 17:31:12 -0300

    Working input validation

--- src/input.rs
+++ src/input.rs
@@ -1,29 +1,38 @@
-use std::io::{self, stdin};
+use std::io::stdin;
 
 pub struct Input {
-    pub buffer: char,
-    pub size: usize
+    pub input: char,
 }
 
 enum InputError {
     IncorrectSize,
-    Char
+    InvalidType
 }
 
 impl Input {
+    /// The function only returns a valid Input, but loops and repeats the validation if it is not
+    /// yet valid.
     pub fn new() -> Self {
         let mut buffer = String::new();
         loop {
             let result = stdin().read_line(&mut buffer);
             match result {
-                Ok(size) => return Self { buffer: String::from(buffer.trim()), size },
+                Ok(size) => {
+                    // Removing new line character
+                    buffer = buffer.trim().to_string();
+                    match validate_input(&mut buffer, size) {
+                        Ok(char) => return Self { input: char },
+                        Err(err) => print_input_error(err),
+                    } 
+                },
                 Err(_) => println!("Invalid input. Please try again"),
             };
         } 
     }
 
+    /// Checks if the chars are equal.
     pub fn equals(&self, s: char) -> bool {
-        s == self.buffer
+        s == self.input
     }
 }
 
@@ -33,15 +42,34 @@ impl Default for Input {
     }
 }
 
-fn validate_input(buffer: String, size: usize) -> Result<char, InputError> {
-   if size != 1 {
-       return Err(InputError::IncorrectSize);
-   } 
-   let char = match buffer.chars().nth(0) {
-       Some(char) => char,
-       None => return Err(InputError::IncorrectSize), // TODO: Change this error later
-   };
+/// Private function used to validate the user input, garanteeing it obeys the following rules:
+/// - Is only one char.
+/// - Is alphabetical or exclamation mark
+/// 
+/// It also makes the character lowercase, if not the case already.
+fn validate_input(buffer: &mut str, size: usize) -> Result<char, InputError> {
+    // Taking account for the new line character.
+    if size != 2 {
+        return Err(InputError::IncorrectSize);
+    } 
+    let mut char = match buffer.chars().next() {
+        Some(char) => char,
+        None => return Err(InputError::IncorrectSize), // TODO: Change this error later
+    };
+ 
+    if !char.is_alphabetic() && char != '!' {
+        return Err(InputError::InvalidType);
+    }
+    if char.is_alphabetic() {
+        char::make_ascii_lowercase(&mut char);
+    }
+    Ok(char)
+}
 
-   // TODO: Validate if alphabetical and lowercase
-   Ok(char)
+/// Prints the correct message to the user based on their input mistake.
+fn print_input_error(err: InputError) {
+    match err {
+        InputError::IncorrectSize => println!("Invalid input. Make sure it is a single character."),
+        InputError::InvalidType => println!("Invalid input. Make sure it is alphabetical (or '!' to quit)"),
+    };
 }
